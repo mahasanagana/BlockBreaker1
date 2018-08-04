58fdf4b4c11edc70dccda95c7b031243d17e2464
mahasanagana <maha.sangana8@gmail.com>
2018-08-04 09:00:46 +0530

    Main.java

---
+++ src/BlockBreakerPanel.java
@@ -0,0 +1,26 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class BlockBreakerPanel extends JPanel{
+
+	ArrayList<Block> blocks=new ArrayList<Block>();
+	BlockBreakerPanel()
+	{
+		for(int i=0;i<8;i++){
+	blocks.add(new Block((i*60+2),0,60,25,"blue.png"));
+	for(int i=0;i<8;i++){
+		blocks.add(new Block((i*60+2),0,60,25,"blue.png"));
+		for(int i=0;i<8;i++){
+		blocks.add(new Block((i*60+2),0,60,25,"blue.png"));
+		}
+		for(int i=0;i<8;i++){
+		blocks.add(new Block((i*60+2),0,60,25,"blue.png"));
+	}
+		public void paintComponent(Graphics g)
+		{
+			for(Block b:blocks)
+	b.draw(g,this);
+		}
+		}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+
+public class Main{
+	public static void main(String args)
+	{
+		JFrame frame=new JFrame("BreakBlocker");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(490,600);
+		
+	}
+}
